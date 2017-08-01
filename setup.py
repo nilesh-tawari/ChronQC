752b25915e1ab7823e60e6b8770e3899dc2fc575
nilesh-tawari <tawarinr@gis.a-star.edu.sg>
2017-08-01 17:14:23 +0800

    ChronQC 1.0.2 version changes

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name='chronqc',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="An Open-source Quality Control Monitoring System for Clinical NGS",
+    description="A Quality Control Monitoring System for Clinical Next Generation Sequencing",
     long_description=readme + '\n\n' + history,
     author="Nilesh R. Tawari",
     author_email='tawari.nilesh@gmail.com',
